e5d246d395bcc7e829a5dc187259cbff5693d666
Vimal <vimal.cmexpertise@gmail.com>
2026-02-11 18:02:54 +0530

    commit

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,1 +1,11 @@
 <?php
+function formatFileSize($bytes)
+{
+    if ($bytes >= 1048576) {
+        return round($bytes / 1048576, 2) . ' MB';
+    } elseif ($bytes >= 1024) {
+        return round($bytes / 1024, 2) . ' KB';
+    } else {
+        return $bytes . ' B';
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,4 +18,27 @@ class HomeController extends Controller
         $ImageList = Image::get();
         return view('welcome', compact('title', 'categoryList', 'ImageList', 'js'));
     }
+    public function imageDetail(Request $request)
+    {
+        // $id = decrypt($id);
+        $image = Image::with(['category', 'subcategory', 'collection'])
+            ->findOrFail(38);
+
+        $data = [
+            'id'          => $image->id,
+            'title'       => $image->image_name,
+            'description' => $image->image_description ?? 'No description available',
+            'image_url'   => asset('uploads/images/high/' . $image->high_path),
+            'location'    => $image->subcategory->name ?? '',
+            'resolution'  => $image->width . ' x ' . $image->height,
+            'file_size'   => formatFileSize((int)$image->file_size),
+            'tags'        => explode(',', $image->tags ?? ''),
+            'price'       => $image->image_price,
+            'category'    => $image->category->category_name ?? '',
+            'collection'  => $image->collection->name ?? '',
+        ];
+        dd($data);
+
+        return view('image.show', compact('data'));
+    }
 }
