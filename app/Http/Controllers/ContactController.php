303d42f8da2013110e85b7ba3e0dee6c6a53cfd5
Vimal <vimal.cmexpertise@gmail.com>
2026-02-12 18:06:38 +0530

    commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\ContactMessageMail;
 use App\Models\ContactMessage;
+use App\Models\QuoteRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
@@ -42,6 +43,53 @@ class ContactController extends Controller
 
             DB::commit();
             return back()->with('msg_success', 'Message sent successfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return back()->with('msg_error', 'Something went wrong!');
+        }
+    }
+    public function Quote(Request $request)
+    {
+        $title = 'Home';
+        $page = 'sasa';
+        $js = ['home'];
+
+        return view('front.contact_us', compact('title', 'page', 'js'));
+    }
+    public function quoteStore(Request $request)
+    {
+        try {
+            $request->validate([
+                'first_name'        => 'required|string',
+                'last_name'         => 'required|string',
+                'phone'             => 'required|string',
+                'email'             => 'required|email',
+                'company'           => 'required|string',
+                'job_role'          => 'required|string',
+                'job_function'      => 'required|string',
+                'company_size'      => 'required|string',
+                'country'           => 'required|string',
+                'product_interest'  => 'required|string',
+            ]);
+
+            DB::beginTransaction();
+
+            QuoteRequest::create([
+                'first_name'        => $request->first_name,
+                'last_name'         => $request->last_name,
+                'phone'             => $request->phone,
+                'email'             => $request->email,
+                'company'           => $request->company,
+                'job_role'          => $request->job_role,
+                'job_function'      => $request->job_function,
+                'company_size'      => $request->company_size,
+                'country'           => $request->country,
+                'state'             => $request->state,
+                'product_interest'  => $request->product_interest,
+                'newsletter'        => $request->has('newsletter') ? 1 : 0,
+            ]);
+            DB::commit();
+            return back()->with('msg_success', 'Quote request submitted successfully!');
         } catch (\Exception $e) {
             DB::rollBack();
             return back()->with('msg_error', 'Something went wrong!');
