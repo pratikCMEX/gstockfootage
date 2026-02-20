a9afc93b04e97f22a1f5ad488861057f9c29a493
jilpcmexpertis <jil.p.cmexpertise@gmail.com>
2026-02-20 11:12:47 +0530

    commit

---
+++ resources/views/admin/batchs_img/add_new_img.blade.php
@@ -0,0 +1,52 @@
+<div class="body-wrapper-inner upload-main">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="create-upload-head">
+                            <div class="two-btns">
+                                <button type="button" class="upload-btn btn btn-primary batch-create"><i class="ti ti-plus"></i> Upload</button>
+                                <div class="upload-from-device">
+                                    <div class="upload-from-device-head">
+                                        <button type="button" class="btn upload-close-btn"><i class="fa-solid fa-xmark"></i> Upload</button>
+                                    </div>
+                                    <div class="upload-from-device-content text-center align-items-center">
+                                        <div class="upload-device-content-detail">
+                                            <h2 class="upload-title
+                                        ">Drag and drop your files here or upload from <span class="upload-span">
+                                                    Dropbox
+                                                </span><i class="fa-regular fa-circle-question"></i></h2>
+                                            <button type="button" class="btn btn-secondary btn-upload-device">Upload from device</button>
+
+                                            <div class="apply-default-template">
+                                                <div class="form-check form-switch ps-0">
+                                                    <input class="form-check-input m-0" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                                                    <label class="form-check-label  ps-3" for="flexSwitchCheckDefault ">Apply Default Template input</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="upload-device-list-detail text-start">
+                                            <ul>
+                                                <li>Accepted Video file types:mov.mp4 or mxf</li>
+                                                <li>Maximum file size : 6GB</li>
+                                                <li>Supported browsers : Chrome , firefox ,IE10+ , safari 6+</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-light select-btn">Select All</button>
+                            </div>
+                            <div class="search-filter">
+                                <button class="btn btn-primary search-desktop search-filter-openbtn" id="search_filter">
+                                    <i class="fa-solid fa-magnifying-glass me-3"></i>
+                                    Search and Filter</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
