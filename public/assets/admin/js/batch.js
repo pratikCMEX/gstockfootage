c38af0d75c401c109ed5daeb99cc3993bde8ca93
jilpcmexpertis <jil.p.cmexpertise@gmail.com>
2026-02-23 14:48:21 +0530

    commit

--- public/assets/admin/js/batch.js
+++ public/assets/admin/js/batch.js
@@ -40,41 +40,69 @@ closeupload?.addEventListener("click", function () {
   uploadcontent.classList.remove("active");
 
 });
-
+// upload search filter
+let uploadfiltermobile = document.querySelector("#upload_search_filter_mobile")
 let uploadfilteropen = document.querySelector("#upload_search_filter");
 let uploadfiltercontent = document.querySelector(".upload-search-filter");
 let uploadfilterclose = document.querySelector("#upload_close-filter");
 uploadfilteropen?.addEventListener("click", function () {
   uploadfiltercontent.classList.add("active");
 });
+uploadfiltermobile?.addEventListener("click", function () {
+  uploadfiltercontent.classList.add("mobileactive");
+
+});
 uploadfilterclose?.addEventListener("click", function () {
   uploadfiltercontent.classList.remove("active");
-});
+  uploadfiltercontent.classList.remove("mobileactive");
 
+});
+// img click
 let uploadimages = document.querySelector("#upload_images");
+let nofileselected = document.querySelector(".no-file-selected");
 let uploadimgclose = document.querySelector(".close-select-btn");
+let addmetadata = document.querySelector(".add-metadata")
 let selectimgdrop = document.querySelector(".select-img");
 uploadimages?.addEventListener("click", function () {
   selectimgdrop.classList.add("active");
+  nofileselected.classList.add("active");
+  addmetadata.classList.add("active");
+  viewdata.classList.add("notactive");
 });
 uploadimgclose?.addEventListener("click", function () {
   selectimgdrop.classList.remove("active");
+  nofileselected.classList.remove("active");
+  addmetadata.classList.remove("active");
+  viewdata.classList.remove("notactive");
+
+
 });
 
 const selectedImages = document.querySelectorAll(".upload-image");
-
 selectedImages.forEach((item) => {
   item.addEventListener("click", function () {
-    this.classList.add("selected");
+    this.classList.toggle("selected");
   });
 });
-
 const slider = document.querySelector(".slider");
-
 function updateProgress() {
   const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
   slider.style.setProperty("--progress", value + "%");
 }
-
 slider.addEventListener("input", updateProgress);
 updateProgress();
+
+// view metadata
+let viewdata = document.querySelector(".view-metadata");
+let mobilemetadata = document.querySelector(".mobile-no-file-selcted-btn");
+viewdata?.addEventListener("click", function () {
+  nofileselected.classList.add("showactive");
+});
+addmetadata?.addEventListener("click", function () {
+  nofileselected.classList.add("showaddmetadataactive");
+});
+mobilemetadata?.addEventListener("click", function () {
+  nofileselected.classList.remove("showactive");
+  nofileselected.classList.remove("showaddmetadataactive");
+
+})
