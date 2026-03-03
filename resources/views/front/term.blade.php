0df29f18aca85e2500ffb525ac12daa7a5540bfc
jilpcmexpertis <jil.p.cmexpertise@gmail.com>
2026-03-03 14:20:02 +0530

    commit

---
+++ resources/views/front/term.blade.php
@@ -0,0 +1,13 @@
+<main>
+    <section class="term_section">
+        <div class="container-fluid">
+            <div class="term-content">
+                <div class="heading">
+                    <h2>Terms of <span class="yellow-headings"> Services</span></h2>
+                    <span class="last-update">Last updated: October 29, 2025</span>
+                </div>
+                <p></p>
+            </div>
+        </div>
+    </section>
+</main>
