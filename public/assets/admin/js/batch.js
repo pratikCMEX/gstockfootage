e8b4f7e46b7924746a342171dbb53532d32642d4
Vimal <vimal.cmexpertise@gmail.com>
2026-03-12 20:06:29 +0530

    Update batch.js

--- public/assets/admin/js/batch.js
+++ public/assets/admin/js/batch.js
@@ -598,7 +598,7 @@ $(document).on("click", ".delete-btn-batch", function () {
         if (response.status === true) {
           selectedImages.remove(); // remove only selected
           updateUI();
-          $(".total-files-count").text(response.total + "Items");
+          $(".total-files-count").text(response.total + " Items");
           toastr.success(response.message);
         }
       },
