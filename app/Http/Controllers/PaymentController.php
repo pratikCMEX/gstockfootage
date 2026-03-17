1fb4c56bf99fab0db037c08527c883917ac0bf16
Vimal <vimal.cmexpertise@gmail.com>
2026-03-17 17:09:41 +0530

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -370,7 +370,7 @@ class PaymentController extends Controller
 
         $payload         = $request->getContent();
         $sig_header      = $request->server('HTTP_STRIPE_SIGNATURE');
-        $endpoint_secret = config('services.stripe.webhook_secret');
+        $endpoint_secret = 'whsec_TexQCKeOooKbaoVuJ9LepxWvgWhCvktb';
 
         // ── Verify Stripe signature ───────────────────────────────────────────────
         try {
