75c8d4fb65b92edd631401a3814c92a7acb964af
Vimal <vimal.cmexpertise@gmail.com>
2026-03-18 15:19:38 +0530

    commit

--- resources/views/front/videos.blade.php
+++ resources/views/front/videos.blade.php
@@ -886,7 +886,8 @@
                                             <video class="product-img" width="100%" muted loop playsinline
                                                 preload="auto"
                                                 poster="{{ !empty($video->thumbnail_path) ? Storage::disk('s3')->url($video->thumbnail_path) : asset('assets/admin/images/demo_thumbnail.png') }}">
-                                                <source src="{{ Storage::disk('s3')->url($video->preview_path) }}"
+                                                <source
+                                                    src="{{ $video->preview_path ? Storage::disk('s3')->url($video->preview_path) : Storage::disk('s3')->url($video->mid_path) }}"
                                                     type="video/mp4">
                                             </video>
                                         </a>
