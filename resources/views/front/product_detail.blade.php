90279f91ef45eac2e7f49dc1e32042628719e0a5
Vimal <vimal.cmexpertise@gmail.com>
2026-03-18 15:47:40 +0530

    Update product_detail.blade.php

--- resources/views/front/product_detail.blade.php
+++ resources/views/front/product_detail.blade.php
@@ -79,8 +79,7 @@
                                         style="background-image: url({{ $data['thumbnail'] }})">
                                     </div>
                                 @else
-                                    <img src=" {{ $data['mid_path'] ?? $data['file_url'] }}" class="h-100 w-100"
-                                        alt="">
+                                    <img src=" {{ $data['file_url'] }}" class="h-100 w-100" alt="">
                                     <div class="big-slide-img-overlay"
                                         style="background-image: url({{ $data['low_path'] }})">
                                     </div>
