3fb2d27880acfaf241679f95cf4e9c17472d8778
Vimal <vimal.cmexpertise@gmail.com>
2026-03-19 19:55:39 +0530

    Update home.blade.php

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -384,24 +384,23 @@
 
 
                                         </div>
-                                </div>
-                                </a>
+                                    </a>
 
+                                </div>
+                            @endforeach
+                        @endif
                     </div>
-                    @endforeach
-                    @endif
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-12 text-center">
-        <a href="{{ route('all_photos') }}" class="brows-btn">Browse all content <svg
-                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-chevron-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
-            </svg></a>
-    </div>
+        <div class="col-12 text-center">
+            <a href="{{ route('all_photos') }}" class="brows-btn">Browse all content <svg
+                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-chevron-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                </svg></a>
+        </div>
     </div>
     </div>
 </section>
